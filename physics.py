6499655a2b002d512e84c4608c8337a7206b6ce6
Misza1037 <46529732+Misza1037@users.noreply.github.com>
2021-05-22 19:40:55 +0200

    test

--- physics.py
+++ physics.py
@@ -1,1 +1,9 @@
 #physics.py
+class physBase:
+    def __init__(self, g=10):
+        if type(g) != float or type(g) != int:
+            raise TypeError('physBase.__init__(): >g< variable invalid type')
+        self.g = g
+class physObject:
+    def __init__(self, mass):
+        self.mass = mass
