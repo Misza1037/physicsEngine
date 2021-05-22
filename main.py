a5d66f7075ab727a86968ecd1f466fa1110acd3f
Misza1037 <46529732+Misza1037@users.noreply.github.com>
2021-05-23 01:52:02 +0200

    Physics updated

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from vectors import Vector
 from time import sleep
 screenSize = [700, 700]
 pygame.init()
+dtv = 0.2
 screen = pygame.display.set_mode(screenSize)
-PB = physics.PhysBase(screen, Vector(0.2, 0.2))
-PB.addObject('obj1', physics.PhysObject(_physBase=PB, mass=1, x=350, y=350, color=(0,0,124), r=35))
+PB = physics.PhysBase(screen, screenSize, Vector(dtv, dtv))
+PB.addObject('obj1', physics.PhysObject(base=PB, mass=1, x=350, y=350, color=(0,0,124), r=35))
 #PB.objects['obj1'].applyForce(Vector(0, 1), 'push1')
 #PB.removeObject('obj1')
 activeBackgroundColor = (255, 255, 255)
@@ -17,10 +18,19 @@ while mainLoopRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             mainLoopRunning = False
+        #if event.type == pygame.MOUSEBUTTONDOWN:
+            #print(Vector((PB.objects['obj1'].x - event.pos[0])*0.1, (PB.objects['obj1'].y - event.pos[0])*0.1))
+            #PB.objects['obj1'].implementTemporaryForce(Vector((PB.objects['obj1'].x - event.pos[0])*-0.1, (PB.objects['obj1'].y - event.pos[1])*-0.1))
+    #print(pygame.mouse.get_pressed())
+    if pygame.mouse.get_pressed()[0] == 1:
+        pos = pygame.mouse.get_pos()
+        PB.objects['obj1'].implementTemporaryForce(Vector((PB.objects['obj1'].x - pos[0])*-.015, (PB.objects['obj1'].y - pos[1])*-.015))
     screen.fill(activeBackgroundColor)
     for object in PB.objects:
+        PB.objects[object].isCollidingWithScreenBorder()
         PB.objects[object].cycle()
         PB.objects[object].draw()
+        #if PB.objects[object].v0 != Vector(0,0): print(PB.objects[object].v0)
     pygame.display.flip()
-    sleep(.01)
+    sleep(.005)
 pygame.quit()
--- physics.py
+++ physics.py
@@ -5,7 +5,7 @@ import pygame
 
 class PhysBase:
 
-    def __init__(self, screen, dtv, g = Vector(0, 1)):
+    def __init__(self, screen, screenSize, dtv, g = Vector(0, 1), bCoeff = 0.8):
         if type(g) != Vector:
             raise TypeError('PhysBase.__init__(): g | invalid type')
         self.g = g
@@ -13,7 +13,8 @@ class PhysBase:
         self.nextUsedID = 0
         self.screen = screen
         self.dtv = dtv
-
+        self.screenSize = screenSize
+        self.bCoeff = bCoeff
     def addObject(self, ID, object):
         if ID in self.objects:
             raise ValueError('PhysBase.addObject.(): ID | invalid value | ID already occupied')
@@ -27,60 +28,89 @@ class PhysBase:
 
 class PhysObject:
 
-    def __init__(self, _physBase, mass = 1, x=0, y=0, color=(0,0,0), r=10):
-        if type(_physBase) != PhysBase:
-            raise TypeError('PhysObject.__init__(): _physBase | invalid type')
+    def __init__(self, base, mass = 1, x=0, y=0, color=(0,0,0), r=10):
+        if type(base) != PhysBase:
+            raise TypeError('PhysObject.__init__(): base | invalid type')
         if type(mass) != float and type(mass) != int:
             raise TypeError('PhysObject.__init__(): mass | invalid type')
         self.mass = mass
-        self._physBase = _physBase
+        self.base = base
         self.externalForces = {}
         self.x = x
         self.y = y
         self.color = color
         self.r = r
-        self.v0 = Vector(0,0)
+        self.v0 = Vector(6, 0)
         self.temporaryForce = []
 
     def applyForce(self, forceVector, ID):
         if ID in self.externalForces:
-            if self.externalForces[ID] == None:
-                self.externalForces[ID] = forceVector
-            else:
-                raise ValueError('occupied')#!
+            raise ValueError('PhysObject.applyForce(): ID | invalid value | ID already occupied')#!
         else:
             self.externalForces[ID] = forceVector
 
     def removeForce(self, ID):
         if ID in self.externalForces:
-            self.externalForces[ID] = None
+            self.externalForces.pop(ID)
 
     def acceleration(self):
         externalForcesSum = Vector(0,0)
         for i in self.externalForces:
             if self.externalForces[i] != None:
                 externalForcesSum += self.externalForces[i]
-        #print(externalForcesSum + self._physBase.g)
+        for i in self.temporaryForce:
+            externalForcesSum += i
+        #print(externalForcesSum + self.base.g)
         #print(Vector(1/self.mass, 1/self.mass))
         #print(self.mass)
-        return (externalForcesSum + self._physBase.g) * Vector(1/self.mass, 1/self.mass)
+        return (externalForcesSum + self.base.g) * Vector(1/self.mass, 1/self.mass)
     def velocity(self):
-        rv = self.v0 + self.acceleration() * self._physBase.dtv
-        self.v0 = rv
+        if self.y == self.base.screenSize[1]-self.r:
+            if abs(self.v0.x) < 0.3: self.v0.x = 0
+            if abs(self.v0.y) < 0.3: self.v0.y = 0
+        rv = self.v0 + self.acceleration() * self.base.dtv
+        self.v0 = rv*Vector(0.9995, 0.99995)
+        if abs(self.v0.x) < 0.3 and abs(self.v0.y) < 0.3:
+            self.rv = Vector(0,0)
+            self.v0 = Vector(0,0)
         return rv
     def cycle(self):
-         self.temporaryForce = []
-         vel = self.velocity() * self._physBase.dtv
-         print(vel)
+         vel = self.velocity() * self.base.dtv
          self.x += vel.x
-         self.y += vel.y
+         if self.y != self.base.screenSize[1]-self.r:
+             self.y += vel.y
+         self.temporaryForce = []
     def implementTemporaryForce(self, forceVector):
         self.temporaryForce.append(forceVector)
-    def applyGravitationalForce(self, other):
-        ox = other.x
-        oy = other.y
-        other.implementTemporaryForce(gForce)
+    #def applyGravitationalForce(self, other):
+    #    ox = other.x
+    #    oy = other.y
+    #    other.implementTemporaryForce(gForce)
+
+
+    def isCollidingWithScreenBorder(self):
+        xmax = self.base.screenSize[0]
+        ymax = self.base.screenSize[1]
+        if self.x-self.r <= 0:
+            self.v0 = Vector(-self.v0.x*self.base.bCoeff, self.v0.y)
+            self.x = 1 + self.r
+        elif self.x+self.r >= xmax:
+            self.v0 = Vector(-self.v0.x*self.base.bCoeff, self.v0.y)
+            self.x = xmax - 1 - self.r
+        elif self.y-self.r <= 0:
+            self.v0 = Vector(self.v0.x, -self.v0.y*self.base.bCoeff)
+            self.y = 1 + self.r
+        elif self.y+self.r > ymax:
+            self.v0 = Vector(self.v0.x, -self.v0.y*self.base.bCoeff)
+            self.y = ymax - 1 - self.r
+        elif self.y+self.r > ymax - 2 and self.v0.y < 1:
+            self.v0 = Vector(self.v0.x, 0)
+            self.y=ymax-self.r
+
+
+
+
     def draw(self):
-        pygame.draw.circle(self._physBase.screen, self.color, (int(self.x), int(self.y)), self.r)
+        pygame.draw.circle(self.base.screen, self.color, (int(self.x), int(self.y)), self.r)
 if __name__ == '__MAIN__':
     pass
