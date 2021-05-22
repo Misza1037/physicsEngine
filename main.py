ace547924827fe5d803b14595d54dd6cf8368245
Misza1037 <46529732+Misza1037@users.noreply.github.com>
2021-05-22 22:27:27 +0200

    pygame implemented ## ADDED : ### Physics

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import physics
+import pygame
 from vectors import Vector
-PB = physics.PhysBase()
-PB.addObject('obj1', physics.PhysObject(PB, 0))
-PB.objects['obj1'].mass += 1
-PB.objects['obj1'].applyForce(Vector(0, 1), 'push1')
-print(PB.objects['obj1'])
-print(PB.objects['obj1'].externalForces)
-print(PB.objects['obj1'].mass)
-print(PB.objects['obj1'].acceleration())
-PB.removeObject('obj1')
+from time import sleep
+screenSize = [700, 700]
+pygame.init()
+screen = pygame.display.set_mode(screenSize)
+PB = physics.PhysBase(screen, Vector(0.2, 0.2))
+PB.addObject('obj1', physics.PhysObject(_physBase=PB, mass=1, x=350, y=350, color=(0,0,124), r=35))
+#PB.objects['obj1'].applyForce(Vector(0, 1), 'push1')
+#PB.removeObject('obj1')
+activeBackgroundColor = (255, 255, 255)
+mainLoopRunning = True
+while mainLoopRunning:
+    #pyGame event check
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            mainLoopRunning = False
+    screen.fill(activeBackgroundColor)
+    for object in PB.objects:
+        PB.objects[object].setNewXY()
+        PB.objects[object].draw()
+    pygame.display.flip()
+    sleep(.01)
+pygame.quit()
